d3b467ac1d7bcdc72d6ef5635477fb3d35e0c68d
Yan <ycdssa.snf24@uea.edu.br>
2024-10-01 18:07:29 -0400

    add background.py

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import pygame
 import constants as consts
+import sys
 
 pygame.init()
 
-#sound effects initiation
 pygame.mixer.init()
 
-
-#screen
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 clock = pygame.time.Clock()
+pygame.display.set_caption('Galactic Defenders')
 
-#game loop
+background_image = pygame.image.load("assets/Back_Image/Espace.jpg")
+
+
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
 
-
-    #controls
     key = pygame.key.get_pressed()
     if key[pygame.K_ESCAPE]:
         pygame.quit()
+        sys.exit()
 
+    screen.blit(background_image, (0, 0))
+
+    pygame.display.update()
+
+    clock.tick(60)
