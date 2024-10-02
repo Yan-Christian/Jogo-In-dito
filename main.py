00c8e79ac2443ebf1a685b1eb7f4a2bc7d712f36
Yan <ycdssa.snf24@uea.edu.br>
2024-10-01 21:33:26 -0400

    Add moviments player 1.py

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ pygame.display.set_caption('Galactic Defenders')
 
 background_image = pygame.image.load("assets/Back_Image/Espace.jpg")
 
-player = pygame.image.load("assets/Player_1/Player 1.png")
+player_original = pygame.image.load("assets/Player_1/Player 1.png")
 player_width, player_height = 50, 50
-player = pygame.transform.scale(player, (player_width, player_height))
+player_original = pygame.transform.scale(player_original, (player_width, player_height))
 
 player_x = (consts.WINDOW_WIDTH - player_width) // 2
 player_y = consts.WINDOW_HEIGHT - player_height
+
+player_speed = 5
+
+player = player_original
+player_angle = 0
 
 while True:
 
@@ -26,11 +31,29 @@ while True:
             pygame.quit()
             sys.exit()
 
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_LEFT] and player_x > 0:
+        player_x -= player_speed
+        player_angle = 90
+
+    if key[pygame.K_RIGHT] and player_x < consts.WINDOW_WIDTH - player_width:
+        player_x += player_speed
+        player_angle = -90
+
+    if key[pygame.K_UP] and player_y > 0:
+        player_y -= player_speed
+        player_angle = 0
+
+    if key[pygame.K_DOWN] and player_y < consts.WINDOW_HEIGHT - player_height:
+        player_y += player_speed
+        player_angle = 180
+
+    player_rotated = pygame.transform.rotate(player_original, player_angle)
 
     screen.blit(background_image, (0, 0))
-    screen.blit(player, (player_x, player_y))
+    screen.blit(player_rotated, (player_x, player_y))
 
     pygame.display.update()
-
 
     clock.tick(60)
