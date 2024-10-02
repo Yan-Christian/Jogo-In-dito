d10a1d75a46de99730dbe69f7581ec23f3bc20a7
Yan <ycdssa.snf24@uea.edu.br>
2024-10-02 13:42:31 -0400

    Add sounds and + bullets mechanics

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 import pygame
 import constants as consts
 import sys
-import mechanics as mec
 
 pygame.init()
-
 pygame.mixer.init()
 
+# screen setup
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Galactic Defenders')
 
-#background
+# background
 background_image = pygame.image.load("assets/Back_Image/Espace.jpg")
 
-#player 1
+# player setup
 player_original = pygame.image.load("assets/Player_1/Player 1.png")
 player_width, player_height = 50, 50
 player_original = pygame.transform.scale(player_original, (player_width, player_height))
 player_x = (consts.WINDOW_WIDTH - player_width) // 2
 player_y = consts.WINDOW_HEIGHT - player_height
 player_speed = 5
-player = player_original
 player_angle = 0
 
-#bullet
-bullet = pygame.draw.circle(screen, consts.BULLET_COLOR, (player_x, player_y), consts.BULLET_RADIUS,0)
+# bullet setup
+bullet_speed = 7
+bullet = None
+bullets_left = 4
+max_bullets = 4
+cooldown_time = 2
+reloading = False
+reload_start_time = 0
 
+# load sound effect for shooting and reloading
+shoot_sound = pygame.mixer.Sound("assets/Sound game/Disparo.mp3")
+reload_sound = pygame.mixer.Sound("assets/Sound game/Recarregamento.mp3")
 
-#game loop
+# game loop
 while True:
 
     for event in pygame.event.get():
@@ -36,6 +43,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # Key inputs
     key = pygame.key.get_pressed()
 
     if key[pygame.K_LEFT] and player_x > 0:
@@ -53,16 +61,58 @@ while True:
     if key[pygame.K_DOWN] and player_y < consts.WINDOW_HEIGHT - player_height:
         player_y += player_speed
         player_angle = 180
+
+    # Escape key to quit the game
     if key[pygame.K_ESCAPE]:
         pygame.quit()
-    if key[pygame.K_SPACE]:
-        mec.shoot(player_x, player_y, player_angle)
+        sys.exit()
 
+    # Space key to shoot a bullet, only if not in reload mode and bullets available
+    if key[pygame.K_SPACE] and bullet is None and bullets_left > 0 and not reloading:
+        bullet = [player_x + player_width // 2, player_y]
+        shoot_sound.play()
+        bullets_left -= 1
+
+        # inicia a recarga
+        if bullets_left == 0:
+            reloading = True
+            reload_start_time = pygame.time.get_ticks()
+            reload_sound.play()
+
+    # Se estiver recarregando, verifica o tempo de recarga
+    if reloading:
+        current_time = pygame.time.get_ticks()
+        if current_time - reload_start_time >= cooldown_time * 1000:
+            bullets_left = max_bullets
+            reloading = False
+
+    if bullet:
+        bullet[1] -= bullet_speed
+
+
+        if bullet[1] < 0:
+            bullet = None
+
+    # Rotaciona a nave
     player_rotated = pygame.transform.rotate(player_original, player_angle)
 
-    screen.blit(background_image, (0, 0))
-    screen.blit(player_rotated, (player_x, player_y))
+
+    screen.blit(background_image, (0, 0))  # Fundo
+    screen.blit(player_rotated, (player_x, player_y))  # Nave
+
+
+    if bullet:
+        pygame.draw.circle(screen, consts.BULLET_COLOR, (bullet[0], bullet[1]), consts.BULLET_RADIUS)
+
+    # Mostra a quantidade de balas
+    font = pygame.font.SysFont(None, 36)
+    ammo_text = font.render(f'Balas: {bullets_left}', True, (255, 255, 255))
+    screen.blit(ammo_text, (10, 10))
+
+    if reloading:
+        reload_text = font.render('Recarregando...', True, (255, 0, 0))
+        screen.blit(reload_text, (10, 50))
 
     pygame.display.update()
 
-    clock.tick(60)
+    clock.tick(60) 
